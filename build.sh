a3b9c374d1a2a8b489eb9cc8634fba15eb1d1745
Neukz <kacperneumann1@gmail.com>
2024-12-01 14:55:52 +0100

    Update build script to include all C files

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 # Utility script for compiling the program to uniquely identified executables
 
-SOURCE_FILE_NAME="main"
-OUTPUT_FILE="./builds/${SOURCE_FILE_NAME}_$(date +%s%N)"
-gcc "${SOURCE_FILE_NAME}.c" -o "$OUTPUT_FILE" -lncurses
+OUTPUT_FILE="./builds/main_$(date +%s%N)"
+gcc *.c -o "$OUTPUT_FILE" -lncurses
 
 if [ $? -eq 0 ]; then
     echo "Output file: $OUTPUT_FILE"
